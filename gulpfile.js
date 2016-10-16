fece221e78b8fa848ee674d1c9f38e873e913367
ugo.harry@gmail.com <ugo.harry@gmail.com>
2016-10-16 18:48:24 +1100

    Modified gulpfile tasks, added more packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,58 @@
 var gulp = require ('gulp');
+var gulpLoadPlugins = require ('gulp-load-plugins');
+var browserSync = require ('browser-sync');
+var wiredep = require ('wiredep').stream;
 var uglify = require ('gulp-uglify');
 
-gulp.task('default', function(){
-  gulp.src('js/*.js').pipe(uglify());
+var $ = gulpLoadPlugins();
+
+
+//////////////PATHS/////////////
+var jsFiles = 'js/*.js';
+var jsDest = 'dist/scripts';
+var cssFiles = 'styles/css/*.css';
+var scssFiles = 'styles/scss/*.scss';
+var cssDest = 'dist/styles';
+
+//////////////TASKS/////////////
+
+//concatenate and minify the scripts
+gulp.task('scripts', function(){
+  return gulp.src(jsFiles)
+  .pipe($.plumber())
+  .pipe($.concat('scripts.js'))
+  .pipe($.rename('scripts.min.js'))
+  .pipe($.uglify())
+  .pipe(gulp.dest(jsDest));
 });
 
 
-//CONCATENATE JS FILES
+//convert scss to css ad places them in the (source) css directory
+gulp.task('sass', function() {
+  return gulp.src(scssFiles)
+  .pipe($.plumber())
+  .pipe($.sass().on('error', $.sass.logError))
+  .pipe(gulp.dest('styles/css'));
+});
 
-//CONCATENATE STYLESHEETS
+//join, minify and rename css files, then place in the 'dist' directory
+gulp.task ('styles', function() {
+  return gulp.src(cssFiles)
+  .pipe($.plumber())
+  .pipe($.concat('mainStyle.css'))
+  .pipe($.uglifycss({
+    "uglyComments": true
+  }))
+  .pipe($.rename('mainStyle.min.css'))
+  .pipe(gulp.dest(cssDest));
+});
 
-//MINIFY FILES
 
-//SAVE MINIFIED FILES IN A .min EXTENSION IN A DIFFERENT DIRECTORY
+
+
+
+
+gulp.task('default', function(){
+  gulp.src('js/*.js').
+  pipe(uglify());
+});
