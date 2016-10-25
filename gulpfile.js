0ece618ddce5e6f6b552bc8999e718402c5a5680
ugo.harry@gmail.com <ugo.harry@gmail.com>
2016-10-25 23:38:21 +1100

    Added gulp-inject pligin

--- gulpfile.js
+++ gulpfile.js
@@ -57,6 +57,14 @@ gulp.task('bower', function() {
   .pipe(gulp.dest('dist'));
 });
 
+//inject styles and scripts
+gulp.task('inject', function() {
+  var target = gulp.src('dist/index.html');
+  var source = gulp.src(['dist/scripts/*.js', 'dist/styles/*.css']);
+  target.pipe($.inject(source))
+  .pipe(gulp.dest('dist'));
+});
+
 //clean out redundant file and directories
 gulp.task('clean', del.bind(null, ['dist/scripts/*.js', 'dist/styles/*.css']));
 
